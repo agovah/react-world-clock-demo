f6053b25f1a8be74b367219b2564bd460b18d00a
Nicolas S <sir.ago@gmail.com>
2020-08-03 15:28:25 +0200

    Updates

---
+++ precache-manifest.c094e7b9ee630eb131516bb2a8f6dd91.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "182249b13428b6b7fb34b71963c88c86",
+    "url": "/react-world-clock-demo/index.html"
+  },
+  {
+    "revision": "54e5999baa0aef7b508e",
+    "url": "/react-world-clock-demo/static/css/2.c8e6cc25.chunk.css"
+  },
+  {
+    "revision": "47a16af9235ec50e8f08",
+    "url": "/react-world-clock-demo/static/css/main.654d8047.chunk.css"
+  },
+  {
+    "revision": "54e5999baa0aef7b508e",
+    "url": "/react-world-clock-demo/static/js/2.2a03399f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-world-clock-demo/static/js/2.2a03399f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47a16af9235ec50e8f08",
+    "url": "/react-world-clock-demo/static/js/main.439a17c6.chunk.js"
+  },
+  {
+    "revision": "fe46edfc4edcef28bb1b",
+    "url": "/react-world-clock-demo/static/js/runtime-main.d7f45e22.js"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/react-world-clock-demo/static/media/Roboto-Regular.11eabca2.ttf"
+  }
+]);
